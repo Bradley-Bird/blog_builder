f0c72fbb04761624f2b505f55d1f3620bb26bc81
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-09 12:48:37 -0800

    preview defaults updating to view

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
@@ -7,11 +7,16 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('hello2');
+  const [subtitle, setSubtitle] = useState('henlo');
+  const [font, setFont] = useState("Architect's Daughter");
+  const [align, setAlign] = useState('left');
+  const [text, setText] = useState('henlo');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
+      <Preview title={title} subtitle={subtitle} text={text} />
       <Editor />
     </main>
   );
